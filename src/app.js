acf748fa492fb34ca41df8df5b9302ddb5368be7
Badrinath <chandrabadrinathreddy@gmail.com>
2024-09-19 14:16:19 +0530

    wildcard-error-handling

--- src/app.js
+++ src/app.js
@@ -4,14 +4,15 @@ const app = express();
 
 const { userAuth } = require("./middlewares/auth");
 
-app.post("/user/login" , (req,res) => {
-    res.send("welcome to the application");
-})
-
-app.use("/user", userAuth,(req, res, next) => {
+app.use("/user", (req, res, next) => {
+        throw new Error("haha");
         res.send("welcome to the application");
     }
-)
+);
+
+app.use("/", (err, req, res, next) => {
+    res.status(500).send("please contact administator!");
+})
 
 app.listen(5000, () => {
     console.log(`Server is listening on port 5000`);
