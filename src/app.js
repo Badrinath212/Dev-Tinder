acf1edea9bb79f62e7e1b5297ff87ef5ac004dd3
Badrinath <chandrabadrinathreddy@gmail.com>
2024-09-20 13:15:44 +0530

    dynamic data in signup api

--- src/app.js
+++ src/app.js
@@ -4,6 +4,21 @@ const { connectionDB } = require('./config/database');
 
 const app = express();
 
+const User = require('./models/user');
+
+app.use(express.json());
+
+app.post('/signup', async (req, res) => {
+    const user = new User(req.body);
+    try {
+        await user.save();
+        res.send("User sucessfully registered!");
+    }catch (err) {
+        res.status(400).send("Error while saving the Data!"+err.message);
+    }
+})
+
+
 connectionDB()
     .then( () => {
         console.log("Database connnection sucessfully......");
--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectionDB = async () => {
     await mongoose.connect(
-        "mongodb+srv://chandrabadrinathreddy:2XtRypUdYlVAkejb@node.vzfpw.mongodb.net/"
+        "mongodb+srv://chandrabadrinathreddy:2XtRypUdYlVAkejb@node.vzfpw.mongodb.net/devTinder"
     );
 }
 
